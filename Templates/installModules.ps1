0f300808d6370f0777520a835c05e16589310548
arunendra-chauhan <arunendra.chauhan@globant.com>
2019-01-04 04:08:25 +0530

    updated config file for myscripts

--- Templates/installModules.ps1
+++ Templates/installModules.ps1
@@ -1,1 +1,3 @@
-﻿Install-Module xActiveDirectory, xNetworking, xStorage -Force
+﻿Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module xActiveDirectory, xNetworking, xStorage -Force
