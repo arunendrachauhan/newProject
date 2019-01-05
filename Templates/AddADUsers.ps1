49845b9b1ba5dcd10526f94fd07bfa52df731ede
arunendra-chauhan <arunendra.chauhan@globant.com>
2019-01-06 02:11:20 +0530

    update templates

--- Templates/AddADUsers.ps1
+++ Templates/AddADUsers.ps1
@@ -1,40 +1,28 @@
-﻿#Enter a path to your import CSV file
-$ADUsers = Import-csv C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.*\Downloads\*\ADUsers.csv
+﻿#Enter a path to import User CSV file
+$ADUsers = Import-csv C:\ADUsers.csv
 
 foreach ($User in $ADUsers)
 {
 
-       $Username    = $User.username
-       $Password    = $User.password
-       $Firstname   = $User.firstname
-       $Lastname    = $User.lastname
-    $Department = $User.department
-       $OU           = $User.ou
+        $SAName = $User.SAName
+        $FirstName = $User.FirstName
+        $LastName = $User.LastName
+        $Password = $User.Password
+        $Path = $User.Path
+        $FullName = "$FirstName $LastName"
 
        #Check if the user account already exists in AD
-       if (Get-ADUser -F {SamAccountName -eq $Username})
-       {
-               #If user does exist, output a warning message
-               Write-Warning "A user account $Username has already exist in Active Directory."
-       }
-       else
-       {
-              #If a user does not exist then create a new user account
-          
-        #Account will be created in the OU listed in the $OU variable in the CSV file; don’t forget to change the domain name in the"-UserPrincipalName" variable
-              New-ADUser `
-            -SamAccountName $Username `
-            -UserPrincipalName "$Username@globant.com" `
-            -Name "$Firstname $Lastname" `
-            -GivenName $Firstname `
-            -Surname $Lastname `
-            -Enabled $True `
-            -ChangePasswordAtLogon $True `
-            -DisplayName "$Lastname, $Firstname" `
-            -Department $Department `
-            -Path $OU `
-            -AccountPassword (convertto-securestring $Password -AsPlainText -Force)
-
+       if (Get-ADUser -F {SamAccountName -eq "$SAName"}){
+            
+            #If user does exist, output a warning message
+            Write-Warning "A user account $SAName already exists in Active Directory."
+       } 
+       else {
+            #If a user does not exist then create a new user account
+            New-ADUser -SamAccountName $SAName `
+            -Name $FullName -GivenName $FirstName -Surname $LastName `
+            -Path $Path -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) `
+            -ChangePasswordAtLogon $true -Enabled $true
        }
 }
 $out = Get-ADUser -Filter{name -like "*" } | Select-object Samaccountname,givenname,surname,enabled 
