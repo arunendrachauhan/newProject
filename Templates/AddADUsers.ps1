46106e1769190a0bd9ba9f737b5c5b3f593d74cb
arunendra-chauhan <arunendra.chauhan@globant.com>
2019-01-06 13:55:10 +0530

    hello

--- Templates/AddADUsers.ps1
+++ Templates/AddADUsers.ps1
@@ -1,33 +1,27 @@
 ﻿#Enter a path to import User CSV file
-$ADUsers = Import-csv C:\ADUsers.csv
+$ADUsers = Import-csv C:\users.csv
 
 foreach ($User in $ADUsers)
 {
+    $SAName = $User.SAName
+    $FirstName = $User.FirstName
+    $LastName = $User.LastName
+    $Password = $User.Password
+    $Path = $User.Path
+    $FullName = "$FirstName $LastName"
 
-        $SAName = $User.SAName
-        $FirstName = $User.FirstName
-        $LastName = $User.LastName
-        $Password = $User.Password
-        $Path = $User.Path
-        $FullName = "$FirstName $LastName"
-
-       #Check if the user account already exists in AD
-       if (Get-ADUser -F {SamAccountName -eq "$SAName"}){
-            
-            #If user does exist, output a warning message
-            Write-Warning "A user account $SAName already exists in Active Directory."
-       } 
-       else {
-            #If a user does not exist then create a new user account
-            New-ADUser -SamAccountName $SAName `
-            -Name $FullName -GivenName $FirstName -Surname $LastName `
-            -Path $Path -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) `
-            -ChangePasswordAtLogon $true -Enabled $true
-       }
+    #Check if the user account already exists in AD
+    if (Get-ADUser -F {SamAccountName -eq $SAName}){
+        #If user does exist, output a warning message
+        Write-Warning "A user account $SAName already exists in Active Directory."
+    } 
+    else {
+        #If a user does not exist then create a new user account
+        New-ADUser -SamAccountName $SAName `
+        -Name $FullName -GivenName $FirstName -Surname $LastName `
+        -Path $Path -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) `
+        -ChangePasswordAtLogon $true -Enabled $true
+    }
 }
-$out = Get-ADUser -Filter{name -like "*" } | Select-object Samaccountname,givenname,surname,enabled 
-$Object = New-Object PSObject -Property @{
-"User Name" = $out.givenname
-"SamAccountName" = $out.Samaccountname
-"User Active State" = $out.enabled
-}$Object | Export-Csv C:\userStatus.csv -NoTypeInformation -Encoding UTF8
+$UserInfo = Get-ADUser -Filter{name -like "*" } | Select-object Samaccountname,givenname,surname,enabled 
+$UserInfo | Export-Csv C:\userStatus.csv -NoTypeInformation 
